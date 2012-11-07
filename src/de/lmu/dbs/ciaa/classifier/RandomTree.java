3d5c025699cd907870f34ca996aa78a8014a1dc8
Thomas Weber <tom-vibrant@gmx.de>
2012-11-07 21:49:34 +0100

    Testrun to target: test11

--- src/de/lmu/dbs/ciaa/classifier/RandomTree.java
+++ src/de/lmu/dbs/ciaa/classifier/RandomTree.java
@@ -187,6 +187,7 @@ public class RandomTree extends Thread {
 				
 				System.out.println(i + " " + s);
 				byte[][] spec = s.getSpectrum();
+				System.out.println(spec);
 				classification.set(i, new byte[spec.length][spec[0].length]);
 			}
 		}
