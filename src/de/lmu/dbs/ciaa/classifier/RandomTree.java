1a9a2cf4cc2b67ae1973e09b08281eeb213a4ccb
Thomas Weber <tom-vibrant@gmx.de>
2012-11-10 01:17:26 +0100

    Testrun to target: remote29_freitag_03g

--- src/de/lmu/dbs/ciaa/classifier/RandomTree.java
+++ src/de/lmu/dbs/ciaa/classifier/RandomTree.java
@@ -170,7 +170,7 @@ public class RandomTree extends Tree {
 		if (depth >= maxDepth) {
 			//node.probabilities = calculateLeaf(sampler, classification, mode, depth);
 			node.probability = calculateLeaf2(sampler, classification, mode, depth);
-			if (params.logNodeInfo) Log.write(pre + "Leaf; Probability " + node.probability);
+			if (params.logNodeInfo) Log.write(pre + " Mode " + mode + " leaf; Probability " + node.probability);
 			return;
 		}
 		
@@ -288,7 +288,7 @@ public class RandomTree extends Tree {
 			// No, make leaf and return
 			//node.probabilities = calculateLeaf(sampler, classification, mode, depth);
 			node.probability = calculateLeaf2(sampler, classification, mode, depth);
-			if (params.logNodeInfo) Log.write(pre + "Leaf; Probability " + node.probability);
+			if (params.logNodeInfo) Log.write(pre + " Mode " + mode + " leaf; Probability " + node.probability);
 			return;
 		}
 		
