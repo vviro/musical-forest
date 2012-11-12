1948c1ae6d996914eb875e9e9ef74c340c432b55
Thomas Weber <tom-vibrant@gmx.de>
2012-11-12 23:52:52 +0100

    Testrun to target: remote32_montag6_dep3_Static

--- src/de/lmu/dbs/ciaa/classifier/RandomTree.java
+++ src/de/lmu/dbs/ciaa/classifier/RandomTree.java
@@ -601,6 +601,10 @@ public class RandomTree extends Tree {
 	 */
 	private void saveDebugTreeRec(Node node, int depth, int mode) throws Exception {
 		String nf = params.workingFolder + File.separator + "T" + num + "_Depth" + depth + "_mode_" + mode + "_id_" + node.id + ".png";
+		if (node == null || node.debugTree == null) {
+			System.out.println("ERROR: Could not save image, node: " + node + ", debugTree: " + node.debugTree);
+			return;
+		}
 		SpectrumToImage img = new SpectrumToImage(node.debugTree.length, node.debugTree[0].length);
 		img.add(node.debugTree, Color.YELLOW);
 		img.save(new File(nf));
