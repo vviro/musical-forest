e0b9397c2ea6c7a741a1ed2ad1acefad6c27cf99
Thomas Weber <tom-vibrant@gmx.de>
2012-11-23 04:12:11 +0100

    Testrun to target: remote43_donnerstag1_LR_2_3_dep6_negHarmDurch10

--- src/de/lmu/dbs/ciaa/classifier/features/FeatureOnsetLR_2_3.java
+++ src/de/lmu/dbs/ciaa/classifier/features/FeatureOnsetLR_2_3.java
@@ -108,7 +108,7 @@ public class FeatureOnsetLR_2_3 extends Feature2d {
 		for(int j=0; j<chosenHarmonics.length; j++) {
 			int ny =  y + harmonics[chosenHarmonics[j]];
 			if (ny >= data[0].length) return ret;
-			ret+= d2 * (data[x][ny] - 128/2) * harmonicFactors[j];
+			ret+= d2 * (data[x][ny] - 128/10) * harmonicFactors[j];
 		}
 		return ret;
 	}
