fe3797b8a8aef407ffe11a905011ab23d5f1ee5b
Thomas Weber <tom-vibrant@gmx.de>
2012-11-27 02:13:52 +0100

    Testrun to target: remote50_montag3_LR_2_6_Oso12

--- src/de/lmu/dbs/ciaa/classifier/features/FeatureOnsetLR_2_6.java
+++ src/de/lmu/dbs/ciaa/classifier/features/FeatureOnsetLR_2_6.java
@@ -148,10 +148,13 @@ public class FeatureOnsetLR_2_6 extends Feature2d {
 	 */
 	private void generateHarmonics(final int amount, final double binsPerOctave) {
 		int[] ret = new int[amount];
+		//float[] r = new float[amount];
 		for(int i=0; i<amount; i++) {
-			ret[i] = (int)(binsPerOctave * (Math.log((i)*2) / Math.log(2)));
+			ret[i] = (int)(binsPerOctave * (Math.log((i+2)*2) / Math.log(2) - 1));
+			//r[i] = (float)(Math.log((i+2)*2) / Math.log(2)) - 1;
 		}
-		ArrayUtils.out(ret);
+		//ArrayUtils.out(ret);
+		//System.exit(0);
 		harmonics = ret;
 	}
 
